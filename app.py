7fb05160448b60aba5532d995c4f70a028258f51
Eric Hopkins <eric.on.tech@gmail.com>
2016-03-05 17:27:59 -0500

    implement login

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ from model import user
 
 client = MongoClient()
 api = API()
+
 api.add_route('/v1/user/', user.Base(client))
 api.add_route('/v1/user/{id}/', user.Single(client))
+api.add_route('/v1/user/login/', user.Login(client))
+# TODO: implement token routes
+# api.add_route('/v1/token/', token.Base(client))
+# api.add_route('/v1/token/{id}/', token.Single(client))
--- model/user/__init__.py
+++ model/user/__init__.py
@@ -1,2 +1,3 @@
 from .Base import Base
 from .Single import Single
+from .Login import Login
