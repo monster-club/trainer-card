f1dbf67c660322229299bab8c7a719278161402c
Eric Hopkins <eric.on.tech@gmail.com>
2016-03-05 20:46:08 -0500

    finish base class for tokens

--- model/token/Base.py
+++ model/token/Base.py
@@ -1,3 +1,32 @@
+from bson.json_util import dumps
+from falcon import HTTPUnauthorized
+from json import loads
+from util.authorize import authorize_as
+from util.Insert import insert_object
+from uuid import uuid4
+
+
 class Base:
   def __init__(self, client):
     self.collection = client.trainer_card.token
+    self.create_keys = ['level']
+
+  def on_get(self, req, resp):
+    if authorize_as(req.auth, 'developer'):
+      resp.body = dumps(self.collection.find())
+    else:
+      raise HTTPUnauthorized('unauthorized', 'unauthorized')
+
+  def on_post(self, req, resp):
+    if authorize_as(req.auth, 'developer'):
+      body = loads(req.stream.read().decode('utf-8'))
+      insert = insert_object(self.create_keys, body, True)
+      insert['key'] = str(uuid4()).replace('-', '')
+      insert['used'] = False
+
+      created = self.collection.insert_one(insert)
+
+      resp.status = HTTP_201
+      resp.body = dumps({'id': created.inserted_id})
+    else:
+      raise HTTPUnauthorized('unauthorized', 'unauthorized')
