51c9265dea15bc49f179064be166111c9dec57d3
Eric Hopkins <eric.on.tech@gmail.com>
2016-03-14 04:32:51 -0400

    add PUT to allowed verbs
    I hadn't realized that PUT was not allowed without
    being explicitly set.

--- util/cors.py
+++ util/cors.py
@@ -1,4 +1,5 @@
 def pre_cors(req, resp, params):
+  resp.set_header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
   resp.set_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
 def post_cors(req, resp):
