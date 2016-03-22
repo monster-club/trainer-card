a1ee615ca5d131eea34181b05f9c3b0ac20fb7e7
Eric Hopkins <eric.on.tech@gmail.com>
2016-03-21 21:52:05 -0400

    Tests for Token model

--- test/token_test.py
+++ test/token_test.py
@@ -5,8 +5,10 @@ from pytest import yield_fixture
 
 
 client = MongoClient()
-collection = client.trainer_card.token
+collection = client.trainer_card.token_test
 token = Token(client)
+# use a test database
+token.collection = collection
 
 @yield_fixture(autouse=True)
 def tear_down_db():
@@ -24,3 +26,23 @@ def test_get_all():
   for x in range(5):
     collection.insert_one({'level': 'player', 'goodies': x})
   assert token.all().count() == 5
+
+def test_find_by_key():
+  resource = token.create({'level': 'developer', 'goodies': 1})
+  found = token.find(collection.find_one({'_id': resource.inserted_id})['key'])
+  assert found['_id'] == resource.inserted_id
+
+def test_update():
+  resource = token.create({'level': 'developer', 'goodies': 1})
+  before = collection.find_one({'_id': resource.inserted_id})
+  token.update({'used': True}, str(resource.inserted_id))
+  after = collection.find_one({'_id': resource.inserted_id})
+  assert before['used'] == False
+  assert after['used'] == True
+
+def test_delete():
+  resource = token.create({'level': 'developer', 'goodies': 1})
+  result = token.delete(str(resource.inserted_id))
+  record = collection.find_one({'_id': resource.inserted_id})
+  assert result.deleted_count == 1
+  assert record == None
